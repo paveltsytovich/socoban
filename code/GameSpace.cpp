dfc3e1ee39950b17fa0a73bfd5e40640057879b2
EgorShishkin <egor.shishkin.1996@mail.ru>
2017-05-17 00:14:37 +0700

    Реализован метод GetGameObject

--- code/GameSpace.cpp
+++ code/GameSpace.cpp
@@ -12,6 +12,11 @@ GameSpace::~GameSpace()
 
 GameObject* GameSpace::GetGameObject(int x,int y)
 {
+	for (GameObject* currentObject : GameObjects)
+	{
+		if (currentObject->IsPlace(x, y))
+			return currentObject;
+	}
 	return nullptr;
 }
 void GameSpace::AddGameObject(GameObject *object)
