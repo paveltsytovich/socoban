137189949408acdaae599f57fa0dac55eb0142a1
EgorShishkin <egor.shishkin.1996@mail.ru>
2017-05-17 00:22:26 +0700

    Добавлен тест для Wall

--- code/GameSpaceTest.cpp
+++ code/GameSpaceTest.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "Box.h"
+#include "Wall.h"
 #include "GameSpace.h"
 TEST(GameSpaceTest, AddBoxToGameSpace)
 {
@@ -7,5 +8,13 @@ TEST(GameSpaceTest, AddBoxToGameSpace)
 	GameSpace* g = new GameSpace;
 	g->AddGameObject(b);
 	GameObject* actual=g->GetGameObject(1,2);
+	ASSERT_TRUE(actual != nullptr);
+}
+TEST(GamespaceTest, AddWallToGameSpace)
+{
+	Wall* w = new Wall(1, 5, 10, 20);
+	GameSpace* g = new GameSpace;
+	g->AddGameObject(w);
+	GameObject* actual = g->GetGameObject(3, 7);
 	ASSERT_TRUE(actual != nullptr);
 }
