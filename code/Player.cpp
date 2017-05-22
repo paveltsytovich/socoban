0e4da978a1a3318d0ad544a4f1807a068578155c
EgorShishkin <egor.shishkin.1996@mail.ru>
2017-05-22 23:00:30 +0700

    --

--- code/Player.cpp
+++ code/Player.cpp
@@ -18,4 +18,14 @@ bool Player::CheckCollision(int x, int y)
 void Player::Move(int newX, int newY)
 {
 	GameObject *go = GetGameSpace()->GetGameObject(newX, newY);
+	if (go != nullptr)
+		return;
+
+	if (CheckCollision(newX, newY))
+		return;
+	int x=GetX(), y=GetY();
+	
+	go->Move(newX, newY);
+
+		
 }
