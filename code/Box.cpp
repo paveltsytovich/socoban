2f1196952ff1faeae01a2b084ad44cda433900ae
EgorShishkin <egor.shishkin.1996@mail.ru>
2017-05-26 01:01:43 +0700

    Сделаны методы Move и CheckCollision

--- code/Box.cpp
+++ code/Box.cpp
@@ -14,11 +14,45 @@ Box::~Box()
 
 bool Box::CheckCollision(Direction direction)
 {
+	int x = GetX();
+	int y = GetY();
+	switch (direction)
+	{
+	case Up:
+		y--;
+		break;
+	case Down:
+		y++;
+		break;
+	case Right:
+		x++;
+		break;
+	case Left:
+		x--;
+		break;
+	}
+	GameObject *go = GetGameSpace()->GetGameObject(x, y);
 
-	return true;
+	return go!=nullptr;//Нужно доработать для Target
 }
 
 void Box::Move(Direction direction)
 {
-
+	int x = GetX();
+	int y = GetY();
+	switch (direction)
+	{
+	case Up:
+		SetY(y - 1);
+		break;
+	case Down:
+		SetY(y + 1);
+		break;
+	case Right:
+		SetX(x + 1);
+		break;
+	case Left:
+		SetX(x - 1);
+		break;
+	}
 }
